527293bc65df0fdcbe2e1e018529816503e6b62b
Nishant Poudel <npoudel018@outlook.com>
2022-09-04 10:07:47 +0545

    [Updated]: URL

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    baseUrl:'http://103.94.159.167:8080/BOM/web/index.html#',
+    setupNodeEvents(on, config) {
+      
+      // implement node event listeners here
+    },
+  },
+};
