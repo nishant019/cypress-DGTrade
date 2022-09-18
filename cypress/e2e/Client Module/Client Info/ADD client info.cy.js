9bd4842ad1a382ac062ada20f90e6f8933b14b4a
Nishant Poudel <npoudel018@outlook.com>
2022-09-18 11:59:10 +0545

    Added : client info edit

--- cypress/e2e/Client Module/Client Info/ADD client info.cy.js
+++ cypress/e2e/Client Module/Client Info/ADD client info.cy.js
@@ -10,21 +10,37 @@ describe('Client Module-> Client Info', () => {
   it('Client module-> Client Info Add new User',()=>{
     login()
     cy.visit('/setup/client').wait(900)
-    cy.xpath("//div[@role='button']").contains('Add New Client').click({force:true}) //opens Add new user Modal
+    //opens Add new user Modal
+    cy.xpath("//div[@role='button']").contains('Add New Client').click({force:true})
+    //check if popup modal is visible
     cy.xpath("//div[@class='modal-content']").should('be.visible')
-    cy.xpath("//div[@class='modal-content']//input[@class='_1oYOS FormGroup__calendar disabled']").click()
+    //Registration date
+    cy.xpath("//div[@class='modal-content']//input[@class='_1oYOS FormGroup__calendar disabled']").click() 
       .xpath("//div[@class='_1hh2I _3Rbbx']").contains('20').click()
+    //-------------------------
+    //branch
     cy.xpath("//div[@class='modal-body']//select").eq(1).select('H-O')
+    //Client Type
     cy.xpath("//div[@class='modal-body']//select").eq(2).select('Individual')
+    //NEPSE code
     cy.xpath("//div[@class='modal-body']//input[@name='nepseCode']").type('1122334455678')
+    //Client name
     cy.xpath("//div[@class='modal-body']//input[@name='clientName']").type('Nishant Test Client')
+    //declare dp
     var dp = 10100;
+    //select DP
     cy.xpath("//span[@class='rw-dropdown-list-value']").click()
-      .xpath("//div[@class='rw-popup-container']").contains(dp).click()//.select('H-O')
+      .xpath("//div[@class='rw-popup-container']").contains(dp).click()
+    //---------------------------------
+    //type BOID [130 +var dp + unique number]
     cy.xpath("//div[@class='modal-body']//input[@name='boid']").type('130'+dp+'12345678')
+    //email
     cy.xpath("//div[@class='modal-body']//input[@name='email']").type('np@gm.com')
+    //phone number
     cy.xpath("//div[@class='modal-body']//input[@name='mobileNo']").type('9860476245')
+    //client type
     cy.xpath("//div[@class='modal-body']//select").eq(3).select('Luxury')
+    //Risk type
     cy.xpath("//div[@class='modal-body']//select").eq(4).select('High Risk')
 
   })
